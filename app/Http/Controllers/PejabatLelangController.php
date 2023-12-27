786934adc6c7346313e9dd0dddf7c0b7a9d76545
novaari2 <novaari20@gmail.com>
2023-12-27 23:11:34 +0700

    crud pejabat lelang

--- app/Http/Controllers/PejabatLelangController.php
+++ app/Http/Controllers/PejabatLelangController.php
@@ -2,9 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PejabatLelang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Throwable;
+use Yajra\DataTables\Facades\DataTables;
 
 class PejabatLelangController extends Controller
 {
-    //
+    public function index()
+    {
+        if(request()->ajax()){
+            $data = PejabatLelang::query();
+
+            return DataTables::of($data)
+            ->addIndexColumn()
+            ->addColumn('nama', function($row){
+                return $row->nama;
+            })
+            ->addColumn('nip', function($row){
+                return $row->nip;
+            })
+            ->addColumn('sk_pengangkatan', function($row){
+                return $row->sk_pengangkatan;
+            })
+            ->addColumn('action', function ($row) {
+                $btn = '<a href="' . route('pejabat_lelang.edit', Crypt::encryptString($row->id)) . '" class="edit-categories btn btn-warning-material btn-sm"><i class="mdi mdi-lead-pencil"></i></a>';
+                $btn .= '<button class="delete-pejabat btn btn-danger-material btn-sm ml-1 disabled" data-id=' . Crypt::encryptString($row->id) . ' data-name=' . $row->title . '><i class="mdi mdi-delete"></i></button>';
+                return $btn;
+            })
+            ->rawColumns(['nama','action'])
+            ->make(true);
+        }
+
+        return view('content-dashboard.pejabat_lelang.index');
+    }
+
+    public function destroy(Request $request) {
+        try{
+            $data = PejabatLelang::find(Crypt::decryptString($request->id));
+            $data->delete();
+            return redirect()->route('pejabat_lelang.index')->with('status','Data Berhasil Dihapus');
+        }catch(Throwable $th){
+            return redirect()->route('pejabat_lelang.index')->withErrors($th->getMessage() . ' on the line ' . $th->getLine())->withInput();
+        }
+    }
+
+    public function edit($id){
+        $data = PejabatLelang::find(Crypt::decryptString($id));
+        return view('content-dashboard.pejabat_lelang.edit', compact('data'));
+    }
+
+    public function update(Request $request, $id){
+        try{
+            $data = PejabatLelang::find(Crypt::decryptString($id));
+            $data->update([
+                'nama' => $request->nama,
+                'nip'   => $request->nip,
+                'sk_pengangkatan'   => $request->sk
+            ]);
+
+            return redirect()->route('pejabat_lelang.index')->with('status','Data Berhasil Diubah');
+        }catch(Throwable $th){
+            return redirect()->route('pejabat_lelang.update')->withErrors($th->getMessage() . ' on the line ' . $th->getLine())->withInput();
+        }
+    }
+
+    public function store(Request $request){
+        try{
+            $data = PejabatLelang::create([
+                'nama' => $request->nama,
+                'nip'   => $request->nip,
+                'sk_pengangkatan'   => $request->sk
+            ]);
+
+            return redirect()->route('pejabat_lelang.index')->with('status','Data Berhasil Ditambahkan');
+        }catch(Throwable $th){
+            return redirect()->route('pejabat_lelang.create')->withErrors($th->getMessage() . ' on the line ' . $th->getLine())->withInput();
+        }
+    }
+
+    public function create(){
+        return view('content-dashboard.pejabat_lelang.add');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PejabatLelangController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,13 @@ Route::post('/manage/login', [LoginController::class, 'checkLogin'])->name('mana
 Route::group(['middleware' => 'auth','prefix' => 'administrator','namespace' => 'administrator'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::post('/manage/logout', [LoginController::class, 'logout'])->name('manage.logout');
+
+    Route::group(['namespace' => 'pejabat_lelang', 'prefix' => 'pejabat_lelang'], function(){
+        Route::get('/', [PejabatLelangController::class, 'index'])->name('pejabat_lelang.index');
+        Route::get('/create', [PejabatLelangController::class, 'create'])->name('pejabat_lelang.create');
+        Route::post('/store', [PejabatLelangController::class, 'store'])->name('pejabat_lelang.store');
+        Route::get('edit/{id}', [PejabatLelangController::class, 'edit'])->name('pejabat_lelang.edit');
+        Route::post('update/{id}', [PejabatLelangController::class, 'update'])->name('pejabat_lelang.update');
+        Route::delete('destroy', [PejabatLelangController::class, 'destroy'])->name('pejabat_lelang.destroy');
+    });
 });
