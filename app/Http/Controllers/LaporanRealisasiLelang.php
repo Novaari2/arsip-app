cf4aa31fdda6eacdec1f0a3a8a6216395d6ab836
novaari2 <novaari20@gmail.com>
2024-03-16 05:25:42 +0700

    fixing realisasi lelang per tahun

--- app/Http/Controllers/LaporanRealisasiLelang.php
+++ app/Http/Controllers/LaporanRealisasiLelang.php
@@ -17,30 +17,43 @@ class LaporanRealisasiLelang extends Controller
     public function index()
     {
         if(request()->ajax()){
-            $data = RisalahLelang::all()->groupBy(function($item){
-                return $item->created_at->year;
+            $data = RisalahLelang::with('barang')->get()->groupBy(function($item){
+                return Carbon::parse($item->tgl_register)->format('Y');
             });
+
+            // return response()->json($data);
 
             return DataTables::of($data)
             ->addIndexColumn()
             ->addColumn('tahun', function($row){
-                return Carbon::parse($row->first()->created_at)->format('Y');
+                foreach ($row as $value) {
+                    return date('Y', strtotime($value->tgl_register));
+                }
             })
             ->addColumn('realisasi_pokok_lelang', function($row){
-                $pokok_lelang = Barang::sum('pokok_lelang');
+                $pokok_lelang = 0;
+                foreach ($row as $value) {
+                    $pokok_lelang += $value->barang->where('risalah_lelang_id', $value->id)->sum('pokok_lelang');
+                }
                 return number_format($pokok_lelang, 0, ',', '.');
+                // return '';
             })
             ->addColumn('realisasi_pnbp_lelang', function($row){
-                $bea_jual = Barang::sum('bea_penjual');
-                $bea_beli = Barang::sum('bea_pembeli');
-               $pnbp_lelang = $bea_jual + $bea_beli;
-               return number_format($pnbp_lelang, 0, ',', '.');
+                $bea_penjual = 0;
+                $bea_pembeli = 0;
+                foreach ($row as $value) {
+                    $bea_penjual += $value->barang->where('risalah_lelang_id', $value->id)->sum('bea_penjual');
+                    $bea_pembeli += $value->barang->where('risalah_lelang_id', $value->id)->sum('bea_pembeli');
+                }
+                return number_format($bea_penjual + $bea_pembeli, 0, ',', '.');
             })
             ->addColumn('produktivitas_lelang', function($row){
-                $rlLaku = RisalahLelang::where('st_lelang', 1)->where('created_at', $row->first()->created_at)->get();
-                $rlTap = RisalahLelang::where('st_lelang', 2)->where('created_at', $row->first()->created_at)->get();
+                foreach ($row as $value) {
+                    $rlLaku = $value->where('status_lelang', 1)->whereYear('tgl_register', $value->tgl_register)->get();
+                    $rlTap = $value->where('status_lelang', 2)->whereYear('tgl_register', $value->tgl_register)->get();
+                }
                 $pro_lelang = (count($rlLaku) / (count($rlLaku) + count($rlTap))) * 100;
-                return $pro_lelang . '%';
+                return sprintf('%0.2f', $pro_lelang).'%';
             })
             ->rawColumns(['tahun','realisasi_pokok_lelang','realisasi_pnbp_lelang','produktivitas_lelang'])
             ->make(true);
@@ -52,8 +65,8 @@ class LaporanRealisasiLelang extends Controller
     {
         $template = "template_laporan_realisasi_lelang_pertahun.xlsx";
         $filename = 'Laporan_realisasi_lelang_pertahun' . date('Y-m-d') . '.xlsx';
-        $data = RisalahLelang::all()->groupBy(function($item){
-            return $item->created_at->year;
+        $data = RisalahLelang::with('barang')->get()->groupBy(function($item){
+            return Carbon::parse($item->tgl_register)->format('Y');
         });
         $this->exportExcelRealisasipertahun($data, $filename, $template);
     }
@@ -89,22 +102,26 @@ class LaporanRealisasiLelang extends Controller
 
         $number = 1;
         foreach ($data as $key => $value) {
-            $pokok_lelang = Barang::sum('pokok_lelang');
-            $bea_jual = Barang::sum('bea_penjual');
-                $bea_beli = Barang::sum('bea_pembeli');
-               $pnbp_lelang = $bea_jual + $bea_beli;
+            $pokok_lelang = 0;
+            $bea_pembeli = 0;
+            $bea_penjual = 0;
+            foreach ($value as $key => $item) {
+                $pokok_lelang += $item->barang->where('risalah_lelang_id', $item->id)->sum('pokok_lelang');
+                $bea_penjual += $item->barang->where('risalah_lelang_id', $item->id)->sum('bea_penjual');
+                $bea_pembeli += $item->barang->where('risalah_lelang_id', $item->id)->sum('bea_pembeli');
+                $th = date('Y', strtotime($item->tgl_register));
+                $rlLaku = $item->where('status_lelang', 1)->whereYear('tgl_register', $item->tgl_register)->get();
+                $rlTap = $item->where('status_lelang', 2)->whereYear('tgl_register', $item->tgl_register)->get();
+            }
             $row = 3 + $number;
             $sheet->setCellValue('A' . $row, $number)->getStyle('A' . $row)->getAlignment()->setHorizontal('center');
-            $sheet->setCellValue('B' . $row, Carbon::parse($value->first()->created_at)->format('Y'))->getStyle('B' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('B' . $row, $th)->getStyle('B' . $row)->getAlignment()->setHorizontal('center');
             $sheet->setCellValue('C' . $row, number_format($pokok_lelang, 0, ',', '.'))->getStyle('C' . $row)->getAlignment()->setHorizontal('center');
-            $sheet->setCellValue('D' . $row, number_format($pnbp_lelang, 0, ',', '.'))->getStyle('D' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('D' . $row, number_format($bea_penjual + $bea_pembeli, 0, ',', '.'))->getStyle('D' . $row)->getAlignment()->setHorizontal('center');
 
+            $pro_lelang = (count($rlLaku) / (count($rlLaku) + count($rlTap))) * 100;
 
-            $rlLaku = RisalahLelang::where('st_lelang', 1)->where('created_at', $value->first()->created_at)->get();
-                $rlTap = RisalahLelang::where('st_lelang', 2)->where('created_at', $value->first()->created_at)->get();
-                $pro_lelang = (count($rlLaku) / (count($rlLaku) + count($rlTap))) * 100;
-
-            $sheet->setCellValue('E' . $row, $pro_lelang . '%')->getStyle('E' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('E' . $row, sprintf('%0.2f', $pro_lelang).'%')->getStyle('E' . $row)->getAlignment()->setHorizontal('center');
 
             $sheet->getStyle('A' . $row)->applyFromArray($setStyle);
             $sheet->getStyle('B' . $row)->applyFromArray($setStyle);
