5d7ea146da69ae48e9ac32fc62eb50783f5acebb
novaari2 <novaari20@gmail.com>
2024-03-16 01:15:57 +0700

    fixing laporan risalah lelang

--- app/Http/Controllers/LaporanRisalahLelang.php
+++ app/Http/Controllers/LaporanRisalahLelang.php
@@ -15,27 +15,33 @@ class LaporanRisalahLelang extends Controller
     public function index()
     {
         if(request()->ajax()){
-            $data = RisalahLelang::all()->groupBy(function($item) {
-                return $item->created_at->format('Y');
+            $data = RisalahLelang::select('tgl_register', 'status_lelang')
+            ->get()
+            ->groupBy(function($item) {
+                return \Carbon\Carbon::parse($item->tgl_register)->format('Y');
             });
-            // return response()->json($data);
 
             return DataTables::of($data)
             ->addIndexColumn()
             ->addColumn('tahun', function($row){
-                return Carbon::parse($row->first()->created_at)->format('Y');
+               foreach ($row as $value) {
+                   return date('Y', strtotime($value->tgl_register));
+               }
             })
             ->addColumn('rl_laku', function($row){
-               $laku = RisalahLelang::where('st_lelang', 1)->get();
-               return count($laku);
+                foreach ($row as $value) {
+                    return count($value->where('status_lelang', 1)->whereYear('tgl_register', $value->tgl_register)->get());
+                }
             })
             ->addColumn('rl_tap', function($row){
-                $tap = RisalahLelang::where('st_lelang', 2)->get();
-                return count($tap);
+                foreach ($row as $value) {
+                    return count($value->where('status_lelang', 2)->whereYear('tgl_register', $value->tgl_register)->get());
+                }
             })
             ->addColumn('batal', function($row){
-                $batal = RisalahLelang::where('st_lelang', 4)->get();
-                return count($batal);
+                foreach ($row as $value) {
+                    return count($value->where('status_lelang', 4)->whereYear('tgl_register', $value->tgl_register)->get());
+                }
             })
             ->rawColumns(['tahun','rl_laku','rl_tap','batal'])
             ->make(true);
@@ -47,10 +53,12 @@ class LaporanRisalahLelang extends Controller
     {
         $template = "template_laporan_risalah_lelang.xlsx";
         $filename = 'Laporan_risalah_lelang' . date('Y-m-d') . '.xlsx';
-        $data = RisalahLelang::all()->groupBy(function($item) {
-            return $item->created_at->format('Y');
+        $data = RisalahLelang::select('tgl_register', 'status_lelang')
+        ->get()
+        ->groupBy(function($item) {
+            return \Carbon\Carbon::parse($item->tgl_register)->format('Y');
         });
-        // return response()->json($data);
+        
         $this->exportExcelRisalahLelang($data, $filename, $template);
     }
 
@@ -58,7 +66,6 @@ class LaporanRisalahLelang extends Controller
         // header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment; filename="' . $filename . '"');
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load(public_path('template/' . $template));
-        // dd($spreadsheet);
         $sheet = $spreadsheet->setActiveSheetIndex(0);
         $sheet->setCellValue('A1', 'Laporan Risalah Lelang');
 
@@ -85,12 +92,15 @@ class LaporanRisalahLelang extends Controller
 
         $number = 1;
         foreach ($data as $key => $value) {
-            $laku = RisalahLelang::where('st_lelang', 1)->get();
-            $tap = RisalahLelang::where('st_lelang', 2)->get();
-            $batal = RisalahLelang::where('st_lelang', 4)->get();
+            foreach($value as $v){
+                $laku = $v->where('status_lelang', 1)->whereYear('tgl_register', $v->tgl_register)->get();
+                $tap = $v->where('status_lelang', 2)->whereYear('tgl_register', $v->tgl_register)->get();
+                $batal = $v->where('status_lelang', 4)->whereYear('tgl_register', $v->tgl_register)->get();
+                $th = date('Y', strtotime($v->tgl_register));
+            }
             $row = 3 + $number;
             $sheet->setCellValue('A' . $row, $number)->getStyle('A' . $row)->getAlignment()->setHorizontal('center');
-            $sheet->setCellValue('B' . $row, Carbon::parse($value->first()->created_at)->format('Y'))->getStyle('B' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('B' . $row, $th)->getStyle('B' . $row)->getAlignment()->setHorizontal('center');
             $sheet->setCellValue('C' . $row, count($laku))->getStyle('C' . $row)->getAlignment()->setHorizontal('center');
             $sheet->setCellValue('D' . $row, count($tap))->getStyle('D' . $row)->getAlignment()->setHorizontal('center');
             $sheet->setCellValue('E' . $row, count($batal))->getStyle('E' . $row)->getAlignment()->setHorizontal('center');
@@ -102,6 +112,7 @@ class LaporanRisalahLelang extends Controller
             $sheet->getStyle('E' . $row)->applyFromArray($setStyle);
 
             $number++;
+            // }
         }
 
         // if(ob_get_contents()) ob_end_clean();
