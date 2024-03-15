9f2cf346dee32d4957ff7ea16e4080d633331a6f
novaari2 <novaari20@gmail.com>
2024-03-16 03:03:48 +0700

    fixing laporan realisasi lelang per jenis

--- app/Http/Controllers/LaporanRealisasiLelangJumlah.php
+++ app/Http/Controllers/LaporanRealisasiLelangJumlah.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
 use App\Models\JenisLelang;
 use App\Models\RisalahLelang;
 use Illuminate\Http\Request;
@@ -14,26 +15,38 @@ class LaporanRealisasiLelangJumlah extends Controller
     public function index()
     {
         if(request()->ajax()){
-            $data = RisalahLelang::with('barang','jenisLelang')->get();
-            // return response()->json($data);
+            $data = JenisLelang::with('risalahLelang','risalahLelang.barang')->get();
 
             return DataTables::of($data)
             ->addIndexColumn()
             ->addColumn('nama', function($row){
-                return $row->jenisLelang->nama;
+                return $row->nama;
             })
             ->addColumn('jumlah_lot', function($row){
-                return $row->barang->count();
+                $rsl = $row->risalahLelang;
+                $sum = 0;
+                foreach($rsl as $key => $value){
+                    $sum += $value->barang->count();
+                }
+                return $sum;
             })
             ->addColumn('realisasi_pokok_lelang', function($row){
-                $sum = $row->barang->sum('pokok_lelang');
+                $rsl = $row->risalahLelang;
+                $sum = 0;
+                foreach($rsl as $key => $value){
+                    $sum += $value->barang->where('risalah_lelang_id', $value->id)->sum('pokok_lelang');
+                }
                 return number_format($sum, 0, ',', '.');
             })
             ->addColumn('realisasi_pnbp_lelang', function($row){
-                $bea_penjual = $row->barang->sum('bea_penjual');
-                $bea_pembeli = $row->barang->sum('bea_pembeli');
-                $sum = $bea_penjual + $bea_pembeli;
-                return number_format($sum, 0, ',', '.');
+                $rsl = $row->risalahLelang;
+                $bea_penjual = 0;
+                $bea_pembeli = 0;
+                foreach($rsl as $key => $value){
+                    $bea_penjual += $value->barang->where('risalah_lelang_id', $value->id)->sum('bea_penjual');
+                    $bea_pembeli += $value->barang->where('risalah_lelang_id', $value->id)->sum('bea_pembeli');
+                }
+                return number_format($bea_penjual + $bea_pembeli, 0, ',', '.');
             })
             ->rawColumns(['nama','jumlah_lot','realisasi_pokok_lelang','realisasi_pnbp_lelang'])
             ->make(true);
@@ -45,7 +58,7 @@ class LaporanRealisasiLelangJumlah extends Controller
     {
         $template = "template_laporan_jumlah_realisasi_lelang_perjenis.xlsx";
         $filename = 'Laporan_jumlah_realisasi_lelang_perjenis' . date('Y-m-d') . '.xlsx';
-        $data = RisalahLelang::with('barang','jenisLelang')->get();
+        $data = JenisLelang::with('risalahLelang','risalahLelang.barang')->get();
         $this->exportExcelRealisasiJumlahPerjenis($data, $filename, $template);
     }
 
@@ -80,12 +93,22 @@ class LaporanRealisasiLelangJumlah extends Controller
 
         $number = 1;
         foreach($data as $value){
+            $jml_lot = 0;
+            $pokok_lelang = 0;
+            $bea_penjual = 0;
+            $bea_pembeli = 0;
+            foreach($value->risalahLelang as $key => $item){
+                $jml_lot += $item->barang->count();
+                $pokok_lelang += $item->barang->where('risalah_lelang_id', $item->id)->sum('pokok_lelang');
+                $bea_penjual += $item->barang->where('risalah_lelang_id', $item->id)->sum('bea_penjual');
+                $bea_pembeli += $item->barang->where('risalah_lelang_id', $item->id)->sum('bea_pembeli');
+            }
             $row = 3 + $number;
             $sheet->setCellValue('A' . $row, $number)->getStyle('A' . $row)->getAlignment()->setHorizontal('center');
-            $sheet->setCellValue('B' . $row, $value->jenisLelang->nama)->getStyle('B' . $row)->getAlignment()->setHorizontal('center');
-            $sheet->setCellValue('C' . $row, $value->barang->count())->getStyle('C' . $row)->getAlignment()->setHorizontal('center');
-            $sheet->setCellValue('D' . $row, $value->barang->sum('pokok_lelang'))->getStyle('D' . $row)->getAlignment()->setHorizontal('center');
-            $sheet->setCellValue('E' . $row, $value->barang->sum('bea_penjual') + $value->barang->sum('bea_pembeli'))->getStyle('E' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('B' . $row, $value->nama)->getStyle('B' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('C' . $row, $jml_lot)->getStyle('C' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('D' . $row, number_format($pokok_lelang, 0, ',', '.'))->getStyle('D' . $row)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('E' . $row, number_format($bea_penjual + $bea_pembeli, 0, ',', '.'))->getStyle('E' . $row)->getAlignment()->setHorizontal('center');
 
             $sheet->getStyle('A' . $row)->applyFromArray($setStyle);
             $sheet->getStyle('B' . $row)->applyFromArray($setStyle);
